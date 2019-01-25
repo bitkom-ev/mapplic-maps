ec76c19359ef30c7fee3326a65cabd2ecd41f3e6
konrad tadesse <k.tadesse@bitkom.org>
2019-01-25 09:56:50 +0100

    * mapplic options completed & alphabetical sorted, console.log deactivated;

--- js/mapplic_maps.js
+++ js/mapplic_maps.js
@@ -2,25 +2,25 @@ Drupal.behaviors.InitializeMapplic = {
     attach: function (context, settings) {
         console.log(drupalSettings.mapplic_maps);
         var mapplic = jQuery("#mapplic_maps").mapplic({
-            source: drupalSettings.mapplic_maps.source,
-            clearbutton: drupalSettings.mapplic_maps.clearbutton,
-            sidebar: drupalSettings.mapplic_maps.sidebar,
-            mapfill: drupalSettings.mapplic_maps.mapfill,
-            width: drupalSettings.mapplic_maps.map_width,
-            height: drupalSettings.mapplic_maps.map_height,
-            minimap: drupalSettings.mapplic_maps.minimap,
-            maxscale: drupalSettings.mapplic_maps.maxscale,
-            locations: drupalSettings.mapplic_maps.locations,
-            fullscreen: drupalSettings.mapplic_maps.fullscreen,
-            hovertip: drupalSettings.mapplic_maps.hovertip,
-            search: drupalSettings.mapplic_maps.search,
+            action: 'tooltip',
             animate: drupalSettings.mapplic_maps.animate,
+            clearbutton: drupalSettings.mapplic_maps.clearbutton,
+            height: drupalSettings.mapplic_maps.map_height,
+            hovertip: drupalSettings.mapplic_maps.hovertip,
+            lightbox: false,
+            locations: drupalSettings.mapplic_maps.locations,
+            mapfill: drupalSettings.mapplic_maps.mapfill,
+            maxscale: drupalSettings.mapplic_maps.maxscale,
+            source: drupalSettings.mapplic_maps.source,
+            sidebar: drupalSettings.mapplic_maps.sidebar,
+            width: drupalSettings.mapplic_maps.map_width,
+            minimap: drupalSettings.mapplic_maps.minimap,
+            fullscreen: drupalSettings.mapplic_maps.fullscreen,
+            search: drupalSettings.mapplic_maps.search,
             developer: drupalSettings.mapplic_maps.developer,
             zoom: drupalSettings.mapplic_maps.zoom,
-            lightbox: false,
             zoombuttons: drupalSettings.mapplic_maps.zoombuttons,
-
         });
-        console.log(mapplic);
+     //console.log(mapplic);
     }
 };
